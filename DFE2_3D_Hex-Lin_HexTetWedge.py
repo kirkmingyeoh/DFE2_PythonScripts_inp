d4c6041ec7600cfe42960cff0e7b48eef428abd3
kirkmingyeoh <kirkmingyeoh@gmail.com>
2025-05-23 16:56:56 +0800

    Update DFE2_3D_Hex-Lin_HexTetWedge.py

--- DFE2_3D_Hex-Lin_HexTetWedge.py
+++ DFE2_3D_Hex-Lin_HexTetWedge.py
@@ -460,7 +460,7 @@ for n_macro_eles in range(N_macro_eles): # Loop through all macroscale elements
                 [a2+a4*tsi+a6*zeta+a7*tsi*zeta,b2+b4*tsi+b6*zeta+b7*tsi*zeta,d2+d4*tsi+d6*zeta+d7*tsi*zeta],
                 [a3+a5*tsi+a6*eta+a7*tsi*eta,b3+b5*tsi+b6*eta+b7*tsi*eta,d3+d5*tsi+d6*eta+d7*tsi*eta]]) # Jacobian matrix of the current integration point
        
-        J_RVE = (Weight*abs(np.linalg.det(J)/(B_RVE*H_RVE*T_RVE)))**(1.0/3.0) # Scaling factor for RVE volume at the current integration point
+        J_RVE = (Weight*abs(np.linalg.det(J))/(B_RVE*H_RVE*T_RVE))**(1.0/3.0) # Scaling factor for RVE volume at the current integration point
         
         if round(J_RVE,5) in SF: # Check if this volume scaling factor has been used before, reuse the same RVE Part if so 
             Ind_SF = SF.index(round(J_RVE,5)) # Index of the first instance of this volume scaling factor
@@ -618,7 +618,7 @@ for n_macro_eles in range(N_macro_eles): # Loop through all macroscale elements
                 [a2+a4*tsi+a6*zeta+a7*tsi*zeta,b2+b4*tsi+b6*zeta+b7*tsi*zeta,d2+d4*tsi+d6*zeta+d7*tsi*zeta],
                 [a3+a5*tsi+a6*eta+a7*tsi*eta,b3+b5*tsi+b6*eta+b7*tsi*eta,d3+d5*tsi+d6*eta+d7*tsi*eta]]) # Jacobian matrix of the current integration point
         J_inv = np.linalg.inv(J) # Inverse of the Jacobian matrix of the current integration point
-        J_RVE = (Weight*abs(np.linalg.det(J)/(B_RVE*H_RVE*T_RVE))**(1.0/3.0) # Scaling factor for RVE volume at the current integration point
+        J_RVE = (Weight*abs(np.linalg.det(J))/(B_RVE*H_RVE*T_RVE))**(1.0/3.0) # Scaling factor for RVE volume at the current integration point
 
         # Shape function values at the current macroscale integration point
         Shape_fn = Trilin_Interpolation(tsi,eta,zeta)
